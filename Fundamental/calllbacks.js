1a4d44beafedd984c6b62d867aae785a0b590558
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2023-12-30 09:30:58 +0000

    callbacks update

--- Fundamental/calllbacks.js
+++ Fundamental/calllbacks.js
@@ -1,7 +1,11 @@
 // CALLBACKS
 
-/* Callbacks in JavaScript are functions that are passed as arguments to other functions and are executed after the completion of an asynchronous operation or at a specified time. 
-Callbacks are commonly used in scenarios like event handling, AJAX requests, and setTimeout. */
+/* Callbacks in JavaScript are functions that are passed as arguments to other functions 
+and are executed after the completion of an asynchronous operation or at a specified time. 
+Callbacks are commonly used in scenarios like event handling, AJAX requests, and setTimeout. 
+
+You ask it to do some work then give it a mechanism to call you back when you're done. So I'm not waiting for it to be done.
+*/
 
 
 // Function with a callback parameter
@@ -21,3 +25,38 @@ function sayGoodbye() {
 
 // Using the greet function with a callback
 greet("John", sayGoodbye);
+
+// other callback examples (synchronous)
+
+setTimeout(() =>{
+    console.log("hello")
+}, 100)
+
+
+const names = ['joe', 'jack', 'john']
+names.forEach((name => console.log(name)))
+
+
+const myForEach = (arr, cb) => {
+    for (let i = 0; i < arr.length; i++) {
+        const element = arr[i];
+        cb(element);
+    }
+};
+
+myForEach(names, (name) => {
+
+})
+
+// (asynchronous) doesn't work just a quick example
+
+const loadPokemon = (id, cb) => {
+    fetch(' https://pokeapi.co/api/v2/berry/').then(res => (res.json())).then(data => {
+        cb(data)
+    })
+}
+
+loadPokemon(56, (pokemon) => {
+    console.log();
+}
+)
