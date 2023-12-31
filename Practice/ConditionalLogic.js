17de405a94018952e6d79383f8e69e272b935a5a
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2023-12-31 14:46:31 +0000

    conditional logic set up

---
+++ Practice/ConditionalLogic.js
@@ -0,0 +1,45 @@
+// 1. Check Number:
+// TODO: Write a condition to check if a variable `number` is greater than 10. If true, log "Number is greater than 10."
+
+// 2. Even or Odd:
+// TODO: Determine if a given number `num` is even or odd. Log "Even" or "Odd" accordingly.
+
+
+
+// 3. Positive or Negative:
+// TODO: Check if a variable `value` is positive, negative, or zero. Log "Positive," "Negative," or "Zero" accordingly.
+
+
+
+// 4. Largest Number:
+// TODO: Given two numbers `a` and `b`, find and log the larger of the two.
+
+const largerNumber = a > b ? a : b;
+console.log(largerNumber);
+
+// 5. Username Validation:
+// TODO: Check if a variable `username` meets the criteria for a valid username. Log "Valid" or "Invalid" accordingly. (Criteria: Length must be between 5 and 10 characters.)
+
+
+
+// 6. Password Strength:
+// TODO: Check the strength of a password. Log "Weak," "Medium," or "Strong" based on the following criteria: Weak (less than 6 characters), Medium (6 to 10 characters), Strong (more than 10 characters).
+
+
+
+// 7. Leap Year:
+// TODO: Determine if a given year `year` is a leap year. Log "Leap year" or "Not a leap year" accordingly.
+
+
+// 8. Grading System:
+// TODO: Implement a grading system based on a student's score. Log the corresponding grade (A, B, C, D, or F).
+
+
+// 9. Vowel or Consonant:
+// TODO: Check if a given character `letter` is a vowel or consonant. Log "Vowel" or "Consonant" accordingly.
+
+
+
+// 10. Age Group:
+// TODO: Categorize a person's age into different groups. Log "Child," "Teen," "Adult," or "Senior" based on the age range.
+
