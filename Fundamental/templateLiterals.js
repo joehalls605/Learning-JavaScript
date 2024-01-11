579c9c3107610a0773f3b0f6a075ccacd4d69348
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-01-11 18:48:04 +0000

    templateLiterals

--- Fundamental/templateLiterals.js
+++ Fundamental/templateLiterals.js
@@ -1,6 +1,14 @@
 
 //TEMPLATE LITERALS
 
+
+/*
+Template literals are a more flexible way to work with strings.
+They are enclosed with (``) instead of quotes.
+They can include placedholders, called expressions. Indicated by ${}
+These allow you to embed variables or expressions directly into the string.
+*/
+
 const variable = "world";
 const greeting = `Hello, ${variable}`;
 
@@ -8,3 +16,14 @@ const name = "John";
 const age = 25;
 const message = `My name is ${name} and I'm ${age} years old.`;
 console.log(message);
+
+/*
+Template literals also support multi-line strings without the need for escape characters
+*/
+
+const multilineString = `
+This is a multi-line
+string using
+template literals`;
+
+console.log(multilineString);
---
+++ Practice/templateLiterals.js
@@ -0,0 +1,22 @@
+// Create a Greeting Message:
+// TODO: Create a template literal named `greeting` that includes the name variable and a friendly greeting. 
+// Log the result.
+
+const name = "Joe"
+const templateLiteral = `Hello there ${name}`;
+console.log(greeting);
+
+// Generate a URL:
+// TODO: Construct a URL using template literals by combining a base URL and a resource path. Log the complete URL.
+
+const baseURL = "www.google.com"
+const resoucePath = "/home"
+const templateLiteral2 = `${url}${resoucePath}`;
+console.log(templateLiteral2);
+
+// Display Mathematical Equation:
+// TODO: Use template literals to display a mathematical equation, including two variables and the result. Log the equation.
+
+const numOne = 1;
+const numTwo = 2;
+const result = `${numOne} + ${numTwo} = ${numOne + numTwo}`;
