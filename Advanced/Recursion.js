af9239ffb1659f3ff995c8a659a36ff0f560a715
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-01-12 11:36:54 +0000

    Recursion

--- Advanced/Recursion.js
+++ Advanced/Recursion.js
@@ -34,3 +34,73 @@ countDownRecursive(3)
             // it's now at the bottom countDownRecursive(n) function, so it keeps returning out from 0 - 3
             // return, return, return, return
 
+
+// Simple for loop:
+function sumRange(n){
+    let total = 0;
+    for(let i = n; i > 0; i--){
+        total += i;
+    }
+    return total;
+}
+
+// Recursive version:
+function sumRangeRecursive(n){
+    if(n <= 0){
+        return total
+    }
+    return sumRangeRecursive(n - 1, total + n)
+}
+
+// What happens:
+sumRangeRecursive(3, 0)
+    sumRangeRecursive(2, 3)
+        sumRangeRecursive(1, 5)
+            sumRangeRecursive(0, 6)
+            // returns 6, returns, 6, returns 6, returns 6
+
+
+// These functions so far are easier to do with a for loop, but next one is something you would use recursion for.
+
+function printChildren(t){
+
+}
+
+function printChildrenRecursive(t){
+    if(t.children.length === 0){
+        return;
+    }
+    t.children.forEach(child => {
+        console.log(child.name)
+        printChildrenRecursive(child)
+    });
+}
+
+// What happens:
+printChildrenRecursive("John")
+    printChildrenRecursive("Jim")
+        return
+    printChildrenRecursive("Zoe")
+            printChildrenRecursive("Kyle")
+            return
+            printChildrenRecursive("Sophia")
+            return
+    return
+return
+
+const tree = {
+    name: "John",
+    children: [
+        {
+            name: "Jim",
+            children: []
+        },
+        {
+            name: "Zoe",
+            children: [
+                { name: "Kyle", children: []},
+                { name: "Sophia", children: []},
+            ]
+        }
+    ]
+}
