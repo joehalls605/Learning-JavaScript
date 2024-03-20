663fd75d3fb3a374e89bdc3c2c72a04b4f1230a0
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-03-20 09:15:52 +0000

    ternaryOperator

---
+++ Fundamental/ternaryOperator.js
@@ -0,0 +1,21 @@
+/* Ternary operator
+The ternary operator is a compact way to express conditional statements.
+It generally follows the pattern:
+
+var result = condition ? expression1 : expression 2;
+
+condition - this is the expression that evaluates to either true or false
+expression1 - this is the value or expression returned if true
+expression 2 - this is the value or expression returned if the condition is false
+
+*/
+
+var age = 20;
+var message = (age >= 18) ? "You are an adult" : "You are not an adult";
+
+
+var name = "Joe";
+var message2 = (name === "Joe") ? "You are Joe!" : "You are not Joe!";
+
+
+
