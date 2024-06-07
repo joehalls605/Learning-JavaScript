118bb730dff17d55d2d0d1fef896f15c9b36ef62
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-06-07 11:49:35 +0100

    3d arrays set-up, !!

---
+++ Advanced/3darrays.js
@@ -0,0 +1,11 @@
+/*
+ A 2D array, like a spreadsheet, expands this to having rows and columns, allowing you to organize items in a grid.
+
+A 3D array takes this one step further. Instead of just a flat grid, imagine a box of these grids stacked on top of each other.  
+This box has three dimensions:
+
+Depth: The number of grids (layers) in the box.
+Height: The number of rows in each grid.
+Width: The number of columns in each grid.
+
+*/
