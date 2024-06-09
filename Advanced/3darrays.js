4d00f5c74d34386c27fcf80b7679290829565f51
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-06-09 11:51:41 +0100

    3d arrays

--- Advanced/3darrays.js
+++ Advanced/3darrays.js
@@ -9,3 +9,23 @@ Height: The number of rows in each grid.
 Width: The number of columns in each grid.
 
 */
+
+let threeDArray = [
+    [ // First layer (index 0)
+        [1, 2, 3],
+        [4, 5, 6]
+    ],
+    [ // Second layer (index 1)
+        [7, 8, 9],
+        [10, 11, 12]
+    ],
+    [ // Third layer (index 2)
+        [13, 14, 15],
+        [16, 17, 18]
+    ]
+];
+
+//[layer][row][column]
+
+let element = threeDArray[1][0][2];
+console.log(element); // Outputs: 9
