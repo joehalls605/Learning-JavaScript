5f26a33d27d6064d30f3ceb29bccc9e02ddc4467
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-06-10 11:01:38 +0100

    .reducer

---
+++ Fundamental/.reduce.js
@@ -0,0 +1,37 @@
+/*
+The .reduce method is used to reduce an array to a single value by applying a function to each element in the array.
+It processes each element of the array (from left to right) and accumulates a result.
+
+How reduce Works:
+Accumulator: This is the accumulated result of the reduction. It starts with an initial value if provided.
+Current Value: This is the current element being processed in the array
+
+Overview:
+accumulator: The accumulated value previously returned in the last iteration, or initialValue, if supplied.
+currentValue: The current element being processed in the array.
+initialValue: A value to use as the first argument to the first call of the callback function (optional).
+
+*/
+array.reduce((accumulator, currentValue) => {
+    // operation to perform
+  }, initialValue);
+  
+
+// Example
+
+const numbers = [1, 2, 3, 4, 5];
+
+// This defines the reducer function to sum up numbers. It returns the sum of the accumulator and currentValue
+const sumReducer = (accumulator, currentValue) => accumulator + currentValue;
+
+// Use reduce to get the sum of all numbers.
+const sum = numbers.reduce(sumReducer, 0); // sumReducer is the function that will be applied to each element. 0 is initial value. 
+// the accumulator is the first number in the numbers array which is 1
+
+console.log(sum); // Output: 15
+
+const numbers2 = [1,4,5,7,8];
+
+const sumReducer2 = (accumulator, currentValue) => accumulator + currentValue;
+
+const sum2 = numbers2.reduce(sumReducer2, 0);
