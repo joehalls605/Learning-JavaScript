9c88610d8abb2e6e899c90f8e414412a184193f7
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2024-11-25 09:27:18 +0000

    find, includes, map, pop added.

--- MicroProjects/ArrayMethodPracticeApplications/Array Method Explorer/script.js
+++ MicroProjects/ArrayMethodPracticeApplications/Array Method Explorer/script.js
@@ -1,3 +1,7 @@
+/* TO DO:
+Description card stuff
+*/ 
+
 const sampleData = [
     { id: 1, name: "Alice", age: 25, isStudent: true },
     { id: 2, name: "Bob", age: 30, isStudent: false },
@@ -21,10 +25,21 @@ function selectArrayMethod(button){
     if(button.textContent === "filter"){
         filterFunction(sampleData);
     }
+    if(button.textContent === "find"){
+        findFunction(sampleData);
+    }
+    if(button.textContent === "includes"){
+        includesFunction(sampleData);
+    }
+    if(button.textContent === "map"){
+        mapFunction(sampleData);
+    }
 }
 
 
 // ARRAY FUNCTIONS
+
+// charAt()
 function charAtFunction(sampleData){
     sampleData.forEach(element => {
     const character = element.name.charAt(0);
@@ -33,11 +48,46 @@ function charAtFunction(sampleData){
     });
 }
 
+// filter()
 function filterFunction(sampleData){
   const filteredByAge = sampleData.filter(person => person.age > 25);
+  let output = "";
+  filteredByAge.forEach(person => {
+    output += person.name + "\n";
+  });
+
   console.log(filteredByAge);
+  render(output);
   return filteredByAge;
 }
 
 
+// find()
+function findFunction(){
+    const nameToFind = "Eve"
+    const foundName = sampleData.find(function(person){
+        return person.name === nameToFind;
+    });
+    render(foundName.name);
+}
+
+// includes()
+function includesFunction(){
+    const ages = sampleData.map(person => person.age);
+    const includesOutput = ages.includes(30);
+    render(includesOutput);
+}
+
+// map()
+function mapFunction(){
+    const objectsAgeIncrease = sampleData.map(function(person){
+        return person.age + 1; 
+    });
+    render(objectsAgeIncrease);
+}
+
 // ARRAY RENDERING METHOD
+function render(value){
+    const outputElement = document.getElementById("output");
+    outputElement.append(value);
+}
