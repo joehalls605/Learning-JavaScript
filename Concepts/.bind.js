0ec57e87e92d1f051036439d7264748d3611d224
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2025-01-28 14:32:30 +0000

    .bind revision

--- Concepts/.bind.js
+++ Concepts/.bind.js
@@ -5,52 +5,49 @@ In JavaScript, the .bind() method creates a new function with a specific 'this' 
 This is useful for controlling the context (the value of 'this') in which a function is invoked.
 */
 
-/* Example:
-Imagine you have a dog object and you want to use its bark method separately from the dog object.
-If you call dog.bark(), it works fine because 'this' refers to the 'dog' object inside the bark method.
-*/
-
-const dog = {
-  name: 'Buddy',
-  bark: function() {
-      console.log(this.name + ' says woof!');
-  }
+// 1. The Teacher (Alex) Object
+const alex = {
+    name: 'Alex',
+    teach: function() {
+        console.log(this.name + ' is teaching!');
+    }
 };
 
-// Calling bark directly works fine
-dog.bark(); // Output: Buddy says woof!
+// Calling teach directly works fine because 'this' refers to 'alex'
+alex.teach(); // Output: Alex is teaching!
 
-// However, if you assign the bark method to a variable and call it, there is a problem
-const barkFunction = dog.bark;
-barkFunction(); // Output: undefined says woof! (or error in strict mode)
+// 2. But if we pass the teach method around to a variable:
+const teachFunction = alex.teach;
 
-/*
-This happens because 'this' inside 'barkFunction' no longer refers to the dog object but to the global object, 
-which doesn't have a 'name' property.
-*/
+// Now calling teachFunction will result in 'this' being undefined
+teachFunction(); // Output: undefined is teaching!
 
-// To fix this, use bind(). It allows you to explicitly set what 'this' refers to when the function is called.
-const barkFunctionBind = dog.bark.bind(dog);
-barkFunctionBind(); // Output: Buddy says woof!
+// Why does this happen? Because 'this' inside teachFunction now refers to the global object, not Alex.
 
-/*
-Explanation:
-dog.bark: This accesses the bark method of the dog object.
-.bind(dog): This sets 'this' inside the bark method to always refer to the dog object, even when called separately.
-*/
+///////////////////////////////////////////////
 
-// Another example with a different object
-const module = {
-  x: 42,
-  getX: function() {
-      return this.x;
-  }
-};
+// 3. To fix this, we can use .bind() to ensure 'this' always refers to 'alex'
+const boundTeachFunction = alex.teach.bind(alex);
 
-// Assigning the method to a variable and calling it
-const unboundGetX = module.getX;
-console.log(unboundGetX()); // Output: undefined (or error in strict mode)
+// Now calling the new function will always refer to Alex, no matter where it's called
+boundTeachFunction(); // Output: Alex is teaching!
 
-// Using .bind() to ensure 'this' refers to 'module'
-const boundGetX = module.getX.bind(module);
-console.log(boundGetX()); // Output: 42
+
+const joe = {
+    name: "joe",
+    coding: function(){
+        console.log(this.name + "is coding");
+    }
+}
+
+joe.coding(); // Output: joe is coding
+
+const codingFunction = joe.coding;
+codingFunction(); // Output undefined!
+
+// To fix this
+
+const boundCodingFunction = joe.coding.bind(joe); // binding coding function to joe object so they are always connected
+boundCodingFunction();
+
+
