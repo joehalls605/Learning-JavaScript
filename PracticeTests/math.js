3f9e3c0cb451a9786306d9c55e21bf10d187ea66
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2025-02-18 10:46:56 +0000

    math.js practice

---
+++ PracticeTests/math.js
@@ -0,0 +1,69 @@
+// Task 1:
+// Generate a random number between 1 and 100 and round it to the nearest integer.
+// Answer:
+
+const randomNumber = Math.round(Math.random() * 100) +1;
+
+// Task 2:
+// Find the largest number in the array [3, 7, 2, 15, 8].
+// Answer:
+const array = [3, 7, 2, 15, 8];
+const largestNumber = Math.max(...array);
+
+// Task 3:
+// You have a decimal number 5.78. Round it down to the nearest integer.
+// Answer:
+
+const roundedDown = Math.floor(5.78);
+
+// Task 4:
+// Calculate the square root of 144.
+// Answer:
+
+const sqrRoot = Math.sqrt(144);
+
+// Task 5:
+// Generate a random floating-point number between 0 and 1, then multiply it by 100 and round it up to the nearest integer.
+// Answer:
+
+const randomNum = Math.ceil(Math.random() * 100);
+
+// Task 6:
+// You have a decimal number 8.34. Round it up to the nearest integer.
+// Answer:
+
+const deciRoundedUp = Math.ceil(8.34);
+
+Math.max()
+
+// Task 7:
+// You have a decimal number 15.92. Round it down to the nearest whole number.
+
+const roundedDown = Math.floor(15.92);
+
+// Task 8:
+// You have an array of numbers: [5, 12, 3, 9, 21, 8]. Find the smallest number in the array.
+
+const array2 = [5, 12, 3, 9, 21, 8];
+const smallestNumber = Math.min(...array2);
+
+// Task 9:
+// Generate a random number between 50 and 150. Round the result to the nearest integer.
+
+const random = Math.round(Math.random() * (150 - 50) + 50);
+
+// Task 10:
+// You have a number 6. Find the square root of this number.
+
+const sqrt = Math.sqrt(6);
+
+
+// Task 11:
+// You have a decimal number 7.56. Round it up to the nearest integer.
+
+const roundedUp = Math.ceil(7.56);
+
+// Task 12:
+// Generate a random floating-point number between 1 and 10. Round it down to the nearest integer.
+
+const randomFloat = Math.floor(Math.random() * 10) + 1;
