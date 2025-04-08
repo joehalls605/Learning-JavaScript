bc51eeacc5a79c1447d6e7cad10de3bae6ed0aca
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2025-04-08 16:14:41 +0100

    Declaration and data types.

---
+++ ProblemQuestions/Declaration and data types.js
@@ -0,0 +1,40 @@
+const person = {
+    name:"Alice",
+    age:25,
+    favouriteFoods: ["pizza", "sushi", "ice cream"],
+    hasDrivingLicense: true
+};
+
+for(const key in person){
+    console.log(key + ":" + person[key]);
+}
+
+function typeTransformer(){
+    let num = 42;
+    let str = "123";
+    let bool = true;
+    const numAsString = String(num);
+    const strAsNumber = Number(str);
+    const boolAsString = String(bool)
+
+    return {
+        numAsString,
+        strAsNumber,
+        boolAsString
+    };
+}
+typeTransformer();
+
+function checkTypes(){
+    let a = "5" + 3;
+    let b = "10" - 2;
+    let c = true + 1
+    let d = false + "7";
+
+    return {
+        a: { value: a, type: typeof a },
+        b: { value: b, type: typeof b },
+        c: { value: c, type: typeof c },
+        d: { value: d, type: typeof d }
+    }
+}
