e91f7303956089f5e9564837e358b189f5df96e5
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2025-05-06 17:47:22 +0100

    .sort practice problems.

--- ProblemQuestions/sort.js
+++ ProblemQuestions/sort.js
@@ -26,3 +26,34 @@ console.log(sortedByCheapest);
 
 const sortedByExpensive = [...products].sort((a,b) => b.price - a.price);
 console.log(sortedByExpensive);
+
+//
+
+// 1.
+const numbers = [10,20,33,4,1,2];
+numbers.sort((a,b) => a - b); // If it returns negative, a comes before b, if it returns positive, b comes before a.
+
+// 2.
+
+const words = ["Apple", "Pear", "Tomato", "Grape"];
+words.sort((a, b) => a.toLowerCase().localeCompare(b.toLowerCase()));
+
+// 3.
+
+const people = [
+    { name: 'John', age: 25 },
+    { name: 'Sara', age: 22 },
+    { name: 'Kate', age: 30 }
+];
+people.sort((a, b) => a.age - b.age);
+
+// 4.
+const numbers = [34, 12, 5, 99, 7];
+numbers.sort((a,b) => b - a);
+
+// 5.
+
+const words = ['elephant', 'dog', 'cat', 'hippopotamus', 'mouse'];
+words.sort((a, b) => a.length - b.length);
+
+
