3384d09beb8bc6014c3264432e3f966135f676e0
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2025-05-07 13:30:52 +0100

    spread and rest problem practice.

--- ProblemQuestions/spreadAndRest.js
+++ ProblemQuestions/spreadAndRest.js
@@ -35,3 +35,28 @@ const allScores = [...midtermScores, ...finalScores];
 
 const sortedScoresAscending = allScores.sort((a, b) => a - b);
 console.log(sortedScoresAscending);
+
+
+function sumAllNumbers(...nums){
+    return nums.reduce((acc, cur) => {
+        return acc += cur;
+    });
+}
+sumAllNumbers(1,2,3,5,6,3,2);
+
+function highestNumber(...nums){
+    let largestNumber = 0;
+    for(num of nums){
+        if(num > largestNumber){
+            largestNumber = num;
+        }
+    }
+    return largestNumber;
+}
+highestNumber(8,2,14,52,1,2);
+
+const arr1 = [1, 2];
+const arr2 = [3, 4];
+// Create a new array with all values
+// ➞ [1, 2, 3, 4]
+const arr3 = [...arr1, ...arr2];
