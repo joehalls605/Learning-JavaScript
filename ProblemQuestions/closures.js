95f3ee1903f01795aaefc0aa7af5cebb92ffd4f1
joehalls605 <46379660+joehalls605@users.noreply.github.com>
2025-05-08 09:25:07 +0100

    closures practice.

--- ProblemQuestions/closures.js
+++ ProblemQuestions/closures.js
@@ -28,3 +28,54 @@ const greetBob = createGreet("Bob");
 greetAlice();
 greetBob();
 
+
+// 1.
+function counter(){
+    let count = 0;
+
+    function increaseCount(){
+        count++;
+        return count;
+    }
+    return increaseCount;
+}
+
+const increment = counter();
+console.log(increment);
+
+
+// 2.
+
+function multiplyBy(num){
+    function multiply(num){
+        return num * 2;
+    }
+    return multiply;
+}
+const multiplyBy2 = multiplyBy(2);
+console.log(multiplyBy2(5));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
