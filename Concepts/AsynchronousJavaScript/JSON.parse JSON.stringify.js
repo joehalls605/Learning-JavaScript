5f2bc79e5615e1039e8e9b9fc5a43151df51400f
Joseph Halls <46379660+joehalls605@users.noreply.github.com>
2025-06-24 10:16:19 +0100

    JSON practice, parse and stringify

--- Concepts/AsynchronousJavaScript/JSON.parse JSON.stringify.js
+++ Concepts/AsynchronousJavaScript/JSON.parse JSON.stringify.js
@@ -1,11 +1,48 @@
-// JSON.parse converts JSON string into a JavaScript object
+/* JSON -> JavaScript Object:
+   JSON.parse(jsonString)
+   This reads JSON from server or localStorage
 
-const jsonString = '{"name: "Joe", "age":30 "}';
-const person = JSON.parse(jsonString);
-console.log(person.name); // Outputs: John
+   JavaScript -> JSON
+   JSON.stringify(jsObj)
+   Send data to server or store it locally
+ */
 
-// JSON.stringify converts JavaScript object into JSON string
+// JSON -> JavaScript Object
+const jsonString = '{"name: "Alice", "age": 25}';
+const jsObject = JSON.parse(jsonString);
 
-const person = {name: "John", age: 30};
-const jsonString = JSON.stringify(person);
-console.log(jsonString);
+// JavaScript -> JSON
+const user = {
+    name: "Bob",
+    age: 30
+};
+const json = JSON.stringify(user);
+
+
+// Practice
+const dataFromServer = '{"bookingId": "123", "firstName": "John", "attendees": 4}';
+
+// Convert it to a JavaScript object
+const jsonObj = JSON.parse(dataFromServer);
+// Then log bookingId and attendees
+console.log(jsonObj.bookingId + " " + jsonObj.attendees);
+
+
+const user = { name: "Luna", email: "luna@example.com" };
+// 1. Save to localStorage (must stringify)
+localStorage.setItem("user", JSON.stringify(user));
+
+// 2. Load from localStorage
+const stored = localStorage.getItem("user");
+const parsed = JSON.parse(stored);
+
+console.log(parsed.name); // Output: Luna
+
+
+const user = { name: "Luna", email: "luna@example.com" };
+let userGrabbed = localStorage.getItem("user");
+let parsedUser = JSON.parse(userGrabbed);
+
+parsedUser.age = 28;
+localStorage.setItem("user", JSON.stringify(parsedUser));
+
